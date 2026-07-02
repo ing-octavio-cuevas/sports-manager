a2fc08a89e481629e973c953f8fe597776aadc03
Octavio <ing.octavio.cuevas@gmail.com>
2026-07-02 10:39:10 -0600

    82

--- app/routers/partidos.py
+++ app/routers/partidos.py
@@ -115,7 +115,7 @@ def list_partidos(
         query = query.filter(Partido.torneo_id == torneo_id)
     if jornada_id:
         query = query.filter(Partido.jornada_id == jornada_id)
-    return query.order_by(Partido.fecha_hora, Partido.ubicacion_id).all()
+    return query.order_by(Partido.ubicacion_id, Partido.fecha_hora).all()
 
 
 @router.get("/{partido_id}", response_model=PartidoResponse)
